5cf954e1fe8eb19d288b50c4ab1062f77226c808
Brandon <git@bsquid.io>
2019-06-25 09:28:31 -0700

    Add invite command

--- cogs/meta.py
+++ cogs/meta.py
@@ -12,6 +12,13 @@ class Meta(commands.Cog):
         """Displays my intro message."""
         app_info = await self.bot.application_info()
         await ctx.send('Hello! I\'m a robot! {0.name}#{0.discriminator} made me.'.format(app_info.owner))
+        
+    
+    async def invite(self, ctx):
+        """Send invite link"""
+        app_info = await self.bot.application_info()
+        if app_info.bot_public:
+            await ctx.send(f"Here is my invite link: https://discordapp.com/oauth2/authorize?client_id={app_info.id}&scope=bot&permissions=268445696")
 
 
 def setup(bot):
